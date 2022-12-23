e3d6dbd06c7f6be390c408fe5d1261713da1ed82
MadonnaMelad <120129713+MadonnaMelad@users.noreply.github.com>
2022-12-23 23:14:16 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 
-import { Container } from '@mui/material';
+import { Box,Container,Typography } from '@mui/material';
 import{useEffect} from 'react';
 import  {ThemeProvider} from '@mui/system';
 import theme from './styles/theme';
 import Appbar from './components/appbar';
 import Banner from './components/banner';
+import Promotions from "./components/promotions";
+import Products from "./components/products";
+
 
 function App() {
 
@@ -20,6 +23,11 @@ function App() {
 
   <Appbar />
   <Banner/>
+           <Promotions/>
+  <Box disply="flex" justifyContent={"center"} sx={{p:4}} >
+     <Typography variant = "h4">Our Products </Typography>
+  </Box>
+  <Products/>
   
   </Container>
   </ThemeProvider>
