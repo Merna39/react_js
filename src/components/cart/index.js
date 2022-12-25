cb958df2e8a6a47a484ea4d91bd1b200c284213a
Farah-abdalla <121021635+Farah-abdalla@users.noreply.github.com>
2022-12-25 10:14:47 +0200

    Update index.js

--- src/components/cart/index.js
+++ src/components/cart/index.js
@@ -1,5 +1,5 @@
 import { useTheme } from "@mui/material/styles";
-import { Drawer, useMediaQuery } from "@mui/material";
+import { Avatar,Divider, Drawer, Paper,Typography ,useMediaQuery ,Button } from "@mui/material";
 import { Colors } from "../../styles/theme";
 import {useUIContext} from "../../context/Ui";
 import {Box} from "@mui/system";
@@ -17,28 +17,88 @@ const cartContent = cart.map(item=>(
    display = "flex"
    sx={{pt:2,pb:2}}
    alignItems ="start"
-   justifyContext={"space-between"}
-   >
+   justifyContent={"space-between"}>
+    <Avatar src={item.image} sx={{width: 96, height: 96 , mr:2}} />
+    <Box display="flex" flexDirection={"column"}>
+      <Typorgraphy variant="h6">{item.name}</Typorgraphy>
+     {!matches && <Typorgraphy variant="subtitle2">{item.description}</Typorgraphy>}
+   
    </Box>
+   <Typorgraphy variant="body1" justifyContext={"end"}>
+      ${item.price}
+   </Typorgraphy>
    </Box>
-))
+   {matches && <Typorgraphy variant="subtitle2">{item.description}</Typorgraphy>}
+   <Divider variant="inset" />
+   </Box>
+   
+));
    
 
   return(
     <Drawer 
       open={showCart}
+      onClose={() => setShowCart(false)}
       anchor="right"
       PaperProps={{
         sx:{
-          width:500,
+          width: matches ? '100%' :500,
           background:Colors.light_gray,
            borderRaduis:0
 
-        }
+        },
       }}
       >
+          
+          {cart.length > 0 ? 
+
+        <Box
+        sx={{p: 4}}
+        display="flex"
+        justifyContent={"center"}
+        flexDirection="column"
+        alignItems="center"
+        >
+          <Typorgraphy variant="h3" color={Colors.black}>
+            your cart
+          </Typorgraphy>
+           <Typorgraphy variant="h3" color={Colors.black}>
+            {" "}
+          </Typorgraphy>
+
+            <Paper  
+                  elevation={0}
+                  sx={{
+                    mt: 2,
+                    width: '90%',
+                    padding: 4,
+
+                  }}
+                  >
+               {cartContent}
+            </Paper>
+            <Button sx={{ mt: 4}} variant="contained">
+              proceed to payment
+            </Button>
+      
+
+        </Box> 
+        ): (
+        <Box
+        sx={{p: 4,
+        }}
+        display="flex"
+        justifyContent={"center"}
+        flexDirection="column"
+        alignItems="center" 
+            >
+                 <Typography variant={matches ? "h5" : "h3"} color={Colors.black}></Typography>
+                     your cart  is empty!
+          </Box>
+          )}
   
-    {cartContent}
+             <Button onClick={() => setShowCart(false)}>Close</Button>
+    
     </Drawer>
-  )
+  );
 }
