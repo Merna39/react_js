3c20f7543d7c638b65951fc6bd767ae9e546480d
MiraMarkus <121019848+MiraMarkus@users.noreply.github.com>
2022-12-28 23:25:53 +0200

    Update useCart.js

--- src/hooks/useCart.js
+++ src/hooks/useCart.js
@@ -1,6 +1,12 @@
 
 
 function useCart(product){
-
+    const [ cart , setCart]= useUIContext() ;
+    const addToCart = () => {
+        cart.findIndex( c => c.id === product.id ) >= 0
+        ? setCart( cart.filter( c => c.id !== product.id))
+        : setCart( c => [ ...c , product]) ;
+    }
+    
 }
 export default useCart ;
